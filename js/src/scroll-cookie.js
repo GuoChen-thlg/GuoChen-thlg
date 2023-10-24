fddc3aa4afff9c938dd29f7cc43d97ed76d914b2
天火流光 <gc.thlg@gmail.com>
2023-10-24 13:57:53 +0800

    Site updated: 2023-10-24 13:57:53

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 24 2023 11:28:55 GMT+0800 (Central Standard Time)
+// build time:Tue Oct 24 2023 13:57:31 GMT+0800 (Central Standard Time)
 $(document).ready(function(){var rpath=window.location.href.replace(window.location.origin,"");var timeout;$(window).on("scroll",function(){clearTimeout(timeout);timeout=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+rpath,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var cvalues=Cookies.get("scroll-cookie").split("|");if(cvalues[1]==rpath){$(window).scrollTop(cvalues[0])}}});
 //rebuild by neat 
